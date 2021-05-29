28ad479f072da4c0cd53014976f04be79332bb34
tao <626683392@qq.com>
2021-05-29 13:40:43 +0800

    复现ConcurrentHashMap 扩容时死循环的场景

--- src/main/java/com/meetkiki/conrrent/map/test/ConcurrentHashMapTest.java
+++ src/main/java/com/meetkiki/conrrent/map/test/ConcurrentHashMapTest.java
@@ -1,4 +1,4 @@
-package com.meetkiki.conrrent.map;
+package com.meetkiki.conrrent.map.test;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -7,9 +7,12 @@ public class ConcurrentHashMapTest {
     public static void main(String[] args) {
         ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
 
-        for (int i = 0; i < 100; i++) {
-            concurrentHashMap.put("1" + i, "1");
-        }
+        concurrentHashMap.computeIfAbsent("111",k -> {
+            for (int i = 0; i < 100; i++) {
+                concurrentHashMap.put("1" + i, "1");
+            }
+            return null;
+        });
 
         System.out.println(concurrentHashMap.size() + " count");
     }
